a5809e90377c07f155f17820689bdbf95ab8b27d
Devesh <devesh.rajput22@gmail.com>
2017-07-16 18:54:54 +0530

    getting token on UI

--- src/main/java/com/fbgraph/api/http/service/HttpService.java
+++ src/main/java/com/fbgraph/api/http/service/HttpService.java
@@ -29,7 +29,6 @@ public class HttpService implements com.fbgraph.api.http.interfaces.HttpService{
 			oauthURL = new URL(url);
 			HttpsURLConnection connection = (HttpsURLConnection) oauthURL.openConnection();
 			connection.setRequestMethod("GET");
-			connection.setReadTimeout(1000);
 			connection.connect();
 			connection.disconnect();
 		} catch (MalformedURLException e) {
